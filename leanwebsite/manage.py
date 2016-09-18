102a060659429634faaa784430a12910da064617
lerina <icode.lerina@gmail.com>
2016-09-18 00:19:57 -0400

    reorganized to run pytest and update to django1.10

---
+++ leanwebsite/manage.py
@@ -0,0 +1,52 @@
+###
+# SETTINGS ::..
+##
+import sys
+
+from settings import *
+
+
+###
+# VIEWS ::..
+##
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+###
+# URLS ::..
+##
+from django.conf.urls import url
+
+
+urlpatterns = (  # '',
+        url(r'^$', index, name="homepage"),
+)
+
+
+###
+# tests
+##
+def test_hello_world():
+    assert "hello_world" == "hello_world"
+
+###
+# WSGI ::..
+##
+
+from django.core.wsgi import get_wsgi_application
+
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    import django
+
+    from django.core.management import execute_from_command_line
+
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    django.setup()
+    execute_from_command_line(sys.argv)
